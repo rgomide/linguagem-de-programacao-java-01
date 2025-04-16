789769d0b04bf1db4db4584ab4ac70fbc2072048
rgomide <renato.s.gomide@gmail.com>
2025-04-16 13:25:40 -0300

    .

---
+++ 06-atributes-e-metodos-de-classe/src/br/edu/ifgoiano/static_members/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.ifgoiano.static_members;
+
+public class Principal {
+  public static void main(String[] args) {
+    Produto p1 = new Produto("Produto 1", 10.0);
+    Produto p2 = new Produto("Produto 2", 20.0);
+
+    System.out.println("Total de produtos: " + Produto.getTotalProdutos());
+    System.out.println("Valor total do estoque: " + Produto.getValorTotalEstoque());
+  }
+}
---
+++ 06-atributes-e-metodos-de-classe/src/br/edu/ifgoiano/static_members/Produto.java
@@ -0,0 +1,51 @@
+package br.edu.ifgoiano.static_members;
+
+public class Produto {
+  // Atributos de instância
+  private String nome;
+  private double preco;
+
+  // Atributos estáticos
+  private static int totalProdutos = 0;
+  private static double valorTotalEstoque = 0.0;
+
+  public Produto(String nome, double preco) {
+    this.nome = nome;
+    this.preco = preco;
+    totalProdutos++;
+    valorTotalEstoque += preco;
+  }
+
+  // Métodos de instância
+  public void aplicarDesconto(double percentual) {
+    double desconto = preco * (percentual / 100);
+    valorTotalEstoque -= desconto;
+    preco -= desconto;
+  }
+
+  // Métodos estáticos
+  public static int getTotalProdutos() {
+    return totalProdutos;
+  }
+
+  public static double getValorTotalEstoque() {
+    return valorTotalEstoque;
+  }
+
+  // Getters e Setters
+  public String getNome() {
+    return nome;
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(double preco) {
+    this.preco = preco;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
